37e31d5c67f8d8e8e0130f6a33ea782ed138a861
Brett Berry <brettberry@Bretts-MacBook-Air.local>
2015-09-06 14:34:07 -0700

    Made a stopwatch. It works. Don't press start more than once.

--- app/stopwatch.jsx
+++ app/stopwatch.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
+import moment from 'moment';
 
 export default class Stopwatch extends React.Component {
   constructor() {
     super();
+    this.state = {
+      time: 0
+    };
+    this.interval = null;
+  }
+
+  updateTimer() {
+    this.setState({
+      time: this.state.time + 10
+    });
+  }
+
+  startTimer() {
+    this.interval = setInterval(::this.updateTimer, 10);
+
+  }
+
+  stopTimer() {
+    clearInterval(this.interval);
+  }
+
+  resetTimer() {
+    clearInterval(this.interval);
+    this.setState({
+      time: 0
+    });
   }
 
   render() {
-    return <div></div>;
+    var fancyTime = moment(this.state.time).format('mm:ss:SS');
+    return (
+      <div>
+          <h1>{fancyTime}</h1>
+          <button onClick={::this.startTimer}>Start</button>
+          <button onClick={::this.stopTimer}>Stop</button>
+          <button onClick={::this.resetTimer}>Reset</button>
+      </div>
+    );
   }
 
 }
